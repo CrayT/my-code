61a649613198209d2db2c7761ad52e40d0a0f564
徐涛 <xutao@123kanfang.com>
2021-06-02 21:11:12 +0800

    123数据迁移gui界面

---
+++ pyFile/批量Get并处理ProcessData/GUI_Copy_ProcessData.py
@@ -0,0 +1,143 @@
+from wx import * 
+import oss2
+import requests
+import os
+import threading
+import shutil
+
+class MyFrame(Frame):
+    url = "https://webapi.123kanfang.com/v2/user/GetAccessTokenByName?authKey=SinyiTW-430829"
+    stsUrl = "https://webapi.123kanfang.com/v2/Authorization/AliyunSTS?bucket=" + "" + "&Authorization=" + ""
+    bucket_test = "vrhouse-test"
+    testEndPoint = "http://oss-cn-shanghai.aliyuncs.com"
+    endPoints = {
+    "123kanfang":"http://oss-cn-shenzhen.aliyuncs.com",
+    "vrhouse-sixstar":"http://oss-cn-shanghai.aliyuncs.com",
+    "123vrbucket":"http://oss-cn-shanghai.aliyuncs.com",
+    "offweb3d04":"http://oss-cn-beijing.aliyuncs.com",
+    "centanet-123kanfang":"http://oss-cn-beijing.aliyuncs.com",
+    "vrhouse":"http://oss-cn-shanghai.aliyuncs.com",
+    "sinyi-oss":"http://oss-cn-hongkong.aliyuncs.com",
+    "vrhouse-123":"http://oss-cn-shanghai.aliyuncs.com",
+    "vrhouse-jp":"http://oss-ap-northeast-1.aliyuncs.com",
+    "hkbucket-vr":"http://oss-cn-hongkong.aliyuncs.com"
+    }
+    def __init__(self):
+        Frame.__init__(self,None,-1, title="复制", pos=(300,300),size=(600,350))
+        panel = Panel(self,-1)
+        self.button1 = Button(panel, -1,"开始拷贝", pos=(420, 25), size=(100,30))
+        self.button1.Bind(EVT_BUTTON, self.OnReady)
+
+        StaticText(panel, -1, "2021-06", pos=(520,260))
+
+        StaticText(panel, -1, "请输入IDs:", pos = (30,30))
+        text_input1 = TextCtrl(panel, -1, style=TE_MULTILINE, pos = (90,25), size = (300,60))
+        text_input1.SetEditable(True)
+        self.__TextBox1 = text_input1
+
+        StaticText(panel, -1, "Bucket:", pos = (30,100))
+        text_input2 = TextCtrl(panel, -1,  pos = (90,95), size = (100,30))
+        
+        self.__TextBox2 = text_input2
+
+        text_output = TextCtrl(panel, -1,style=TE_MULTILINE , pos = (40,130), size = (450,150))
+        self.__TextOutBox = text_output
+
+        self.InitUi() 
+
+    def GetDomain(bucket):
+
+        pass
+
+    def GetFileList():
+
+        pass
+
+    def GetToken(self,bucket):
+        url = "https://webapi.123kanfang.com/v2/user/GetAccessTokenByName?authKey=SinyiTW-430829"
+        res = requests.get(url).json()
+        if res["state"] == 200:
+            return res["payload"]["token"]
+        return ""
+
+    def GetSts(self, token, bucket):
+        stsUrl = "https://webapi.123kanfang.com/v2/Authorization/AliyunSTS?bucket=" + bucket + "&Authorization=" + token
+        res = requests.get(stsUrl).json()
+        print('sts:\n', res)
+        if res["state"] == 200:
+            return res["payload"]
+        return ""
+    def CopyData(self, hid, bucket, sts, token):
+        Domain = self.endPoints[bucket]
+        Auth = oss2.StsAuth(sts["accessKeyId"], sts["accessKeySecret"], sts["securityToken"])
+        Bucket = oss2.Bucket(Auth, Domain, bucket)
+        
+        TestDomain = self.testEndPoint
+        TestSts = self.GetSts(token, self.bucket_test)
+        TestAuth = oss2.StsAuth(TestSts["accessKeyId"], TestSts["accessKeySecret"], TestSts["securityToken"])
+        TestBucket = oss2.Bucket(TestAuth, TestDomain, self.bucket_test)
+
+        prefix = hid + '/'
+        abPath = os.getcwd()
+        for obj in oss2.ObjectIterator(Bucket, prefix = prefix):
+            subPath = obj.key
+            print('file: ', subPath)
+            subPath.replace("/", "\\")
+            subPath = os.path.dirname(subPath)
+            subPath.replace("/", "\\")
+            self.__TextOutBox.AppendText(obj.key +"\n")
+
+            hidPath = abPath + "\\" + subPath
+            hidPath = hidPath.rstrip("\\")
+
+            if not os.path.exists(hidPath):
+                os.makedirs(hidPath)
+
+            Bucket.get_object_to_file(obj.key, './' + obj.key)
+
+            TestBucket.put_object_from_file(obj.key, './' + obj.key)
+            os.remove('./' + obj.key)
+
+        shutil.rmtree(abPath + "/" + hid)
+        dial=MessageDialog(None,"拷贝完成")
+        dial.ShowModal()
+
+    def OnReady(self, event):
+        bucket = self.__TextBox2.GetValue()
+        hid = self.__TextBox1.GetValue()
+        if not bucket or not hid:
+            dial=MessageDialog(None,"请输入有效数据")
+            dial.ShowModal()
+        print(hid)
+        hids = hid.split('\n')
+        print(hids)
+        #for id in hids:
+            
+        self.OnFire(hids[0], bucket)
+    def OnFire(self, hid, bucket):
+        token = self.GetToken(bucket)
+        sts = self.GetSts(token, bucket)
+        threadJob = threading.Thread(target = self.CopyData, args = (hid, bucket, sts, token))
+        threadJob.start()
+
+    def Start(self, event):
+        bucket = self.__TextBox2.GetValue()
+        hid = self.__TextBox1.GetValue()
+        if not bucket or not hid:
+            dial=MessageDialog(None,"请输入有效数据")
+            dial.ShowModal()
+
+        token = self.GetToken(bucket)
+        sts = self.GetSts(token, bucket)
+        threadJob = threading.Thread(target = self.CopyData, args = (hid, bucket, sts, token))
+        threadJob.start()
+        #self.CopyData(hid, bucket, sts, token)
+    def InitUi(self):
+
+        pass
+
+if __name__ == "__main__":
+    app = App()   
+    myframe = MyFrame()    
+    myframe.Show()    
+    app.MainLoop()
